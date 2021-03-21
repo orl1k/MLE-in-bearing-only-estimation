ddbd70673b00f79c3e15a0f6d1dfd3bbc9c35924
orl1k <orldima@yandex.ru>
2021-03-21 04:05:27 +0300

    Fixed bugs
    Code optimized
    Code cleaned
    New test

--- main.py
+++ main.py
@@ -4,36 +4,52 @@ from project.tests import Tests
 from project.botma import TMA
 
 # Класс для сохранения результатов
-tests = Tests('2')
+tests = Tests("2")
 
 # Пример моделирования
 
 # Создаем наблюдатель
 observer_x, observer_y, observer_course, observer_velocity = 0, 0, 0, 3
-observer = Ship('Наблюдатель', observer_x, observer_y, observer_course,
-                observer_velocity, verbose=True)
+observer = Ship(
+    "Наблюдатель",
+    observer_x,
+    observer_y,
+    observer_course,
+    observer_velocity,
+    verbose=True,
+)
 # Создаем объект
 target_bearing, target_distance, target_course, target_velocity = 0, 20, 45, 10
-target = Ship('Объект', target_bearing, target_distance, target_course,
-              target_velocity, observer, mode='bdcv', verbose=True)
+target = Ship(
+    "Объект",
+    target_bearing,
+    target_distance,
+    target_course,
+    target_velocity,
+    observer,
+    mode="bdcv",
+    verbose=True,
+)
 
 # Моделирование траекторий
 observer.forward_movement(3 * 60)
-observer.change_course(270, 'left', omega=0.5)
+observer.change_course(270, "left", omega=0.5)
 observer.forward_movement(5 * 60)
-observer.change_course(90, 'right', omega=0.5)
+observer.change_course(90, "right", omega=0.5)
 observer.forward_movement(3 * 60)
 
 # Время движения объекта должно совпадать с временем наблюдателя для TMA
-target.forward_movement(len(observer.coords[0])-1)
+target.forward_movement(len(observer.coords[0]) - 1)
 
 # # Рассматривается маневр объекта
 # target.forward_movement(10 * 60)
 # target.change_course(270, 'left', omega=0.5)
 # target.forward_movement(360)
 
-# dict_results = tma.swarm(algorithm_name='Метод N пеленгов',n=10, seeded=True)
-# df = tests.get_df(dict_results)
+# tma = TMA(observer, std=np.radians(0.1))
+# d_r = tma.swarm(n=10, algorithm_name="Метод N пеленгов", p0=[0, 20, 45, 10])
+# df = tests.get_df(d_r)
+# print(df.drop(["П0_ист", "Д0_ист", "К0_ист"], axis=1).round(2))
 
 # dict_results = tma.swarm(n=10, seeded=True, fixed_target=False, fixed_noise=False, p0=[0., 20., 45., 10.])
 
